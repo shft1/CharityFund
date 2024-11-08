00c659985f982c0abcd181475d5f0d0dd0269c0c
alex <alexmal24122004@gmail.com>
2024-11-09 01:18:17 +0300

    review

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.api.routers import main_router
 from app.core.config import settings
-from app.core.init_db import create_first_superuser
+from app.models.init_db import create_first_superuser
 
 app = FastAPI(
     title=settings.env_app_title,
